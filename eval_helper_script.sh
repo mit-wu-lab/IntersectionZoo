9b6a50cc14af077897ec70b2f23baaea503e0589
Vindula Jayawardana <vindulamj@gmail.com>
2024-06-05 23:09:36 -0400

    Release IntersectionZoo

--- eval_helper_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Loading the required module
-source /etc/profile
-module load anaconda/2022a
-
-# Run the script
-python -u code/policy_evaluation.py --dir wd/$EXP_NAME
--- train_helper_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Loading the required module
-source /etc/profile
-module load anaconda/2022a
-
-# Run the script
-python -u code/ppo_training.py --dir wd/$EXP_NAME
